28e59e105ee5eae5a01bdec84065e87c36cfaab5
EC2 Default User <ckckxellex@gmail.com>
2021-08-22 06:11:17 +0000

    0822

--- db/migrate/20210815071742_create_relationships.rb
+++ db/migrate/20210815071742_create_relationships.rb
@@ -1,8 +1,8 @@
 class CreateRelationships < ActiveRecord::Migration[5.0]
   def change
     create_table :relationships do |t|
-      t.references :user, foreign_key: true
-      t.references :follow, foreign_key: { to_table: :users }
+      t.bigint :user_id, foreign_key: true
+      t.bigint :follow_id, foreign_key: { to_table: :users }
 
       t.timestamps
 
